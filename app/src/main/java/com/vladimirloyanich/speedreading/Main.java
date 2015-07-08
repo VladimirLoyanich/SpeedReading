f640d9bf5cbea04cbbdea6eb5b1a3718deae1b2a
VladimirLoyanich <VladimirLoyanich@gmail.com>
2015-07-08 21:26:28 +0300

    commit

--- app/src/main/java/com/vladimirloyanich/speedreading/Main.java
+++ app/src/main/java/com/vladimirloyanich/speedreading/Main.java
@@ -1,18 +1,10 @@
 package com.vladimirloyanich.speedreading;
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.SeekBar;
 import android.widget.TextView;
 
 
@@ -39,21 +31,21 @@ public class Main extends ActionBarActivity implements UserDialogFragment.Positi
             }
         });
     }
-
+       //filling the username
     public void onPositiveResult(CharSequence text) {
         if(!text.toString().equals("") ){
             UserNameTextView.setText(text);
             enteredEditText=true;}
         else UserNameTextView.setText(R.string.username);
     }
-
+    //call action Test Speed Reading
     public void TestSpeedReading(View view){
         Intent intent = new Intent(this,Speed_reading.class);
         String UserName=UserNameTextView.getText()+"";
         intent.putExtra("com.vladimirloyanich.speedreading.username",UserName);
         startActivity(intent);
     }
-
+    //call action Settings menu
     public void Settings(View view){
         Intent intent1 = new Intent(this,Settings.class);
         startActivity(intent1);
