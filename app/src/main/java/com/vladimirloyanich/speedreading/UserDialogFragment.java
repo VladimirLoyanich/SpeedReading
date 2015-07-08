8d18b775ccba422578a043bc37149d7a6c6f10db
VladimirLoyanich <VladimirLoyanich@gmail.com>
2015-07-08 21:37:51 +0300

    commit dialog

--- app/src/main/java/com/vladimirloyanich/speedreading/UserDialogFragment.java
+++ app/src/main/java/com/vladimirloyanich/speedreading/UserDialogFragment.java
@@ -6,10 +6,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.ActionBarActivity;
-import android.text.Layout;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
