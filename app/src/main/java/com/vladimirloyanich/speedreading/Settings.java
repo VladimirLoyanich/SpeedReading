22ea0c0e8df3832c84ede6e50c8f3e5aac2cd591
VladimirLoyanich <VladimirLoyanich@gmail.com>
2015-07-08 21:35:36 +0300

    commit settings

--- app/src/main/java/com/vladimirloyanich/speedreading/Settings.java
+++ app/src/main/java/com/vladimirloyanich/speedreading/Settings.java
@@ -1,12 +1,8 @@
 package com.vladimirloyanich.speedreading;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
